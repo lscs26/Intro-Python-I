e55b84c4594cbdf3dfaa224d636674aa75929bed
kenna <kennaalastair@gmail.com>
2020-02-11 16:10:30 -0800

    files 8-11 completed

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,23 +9,23 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-print sys.argv
+print(sys.argv)
 
 # Print out the OS platform you're using:
-print sys.platform
+print(sys.platform)
 
 # Print out the version of Python you're using:
-print sys.version
+print(sys.version)
 
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-print os.getpid()
+print(os.getpid())
 
 # Print the current working directory (cwd):
-print os.getcwd()
+print(os.getcwd())
 
 # Print out your machine's login name
-print os.getlogin()
+print(os.getlogin())
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,13 +35,27 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append( [{
+    'lat':'50',
+    'lon':'-138',
+    'name':'a fourth place'
+}] )  
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
+# from Brian, another way to do it
+waypoints[0]['lon'] = -130
+waypoints[0]['name'] = 'not a real place'
 
 # YOUR CODE HERE
+# waypoints[0].update({"lon": -130})
+# waypoints[0].update({"name": "not a real place"})
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for x in waypoints:
+    print(x.values())
